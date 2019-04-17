126f50efb23f3484bc502f1cc817e4a289182c47
Kevin Green <kevin.e.green@hotmail.com>
2019-04-16 20:53:20 -0500

    Recap 2

---
+++ Software Engineer Fundamentals/recap_exercise_1/lib/problemss.rb
@@ -0,0 +1,23 @@
+
+require "byebug"
+class String
+    # Write a method, String#caesar_cipher, that takes in an a number.
+    # The method should return a new string where each char of the original string is shifted
+    # the given number of times in the alphabet.
+    #
+    # Examples:
+    #
+    # "apple".caesar_cipher(1)    #=> "bqqmf"
+    # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
+    # "zebra".caesar_cipher(4)    #=> "difve"
+    def caesar_cipher(num)
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        ret = ""
+        self.each_char do |char|
+            ret += alphabet[(alphabet.index(char)+num)%26]
+        end
+        ret
+    end
+end
+
+p "apple".caesar_cipher(1)
