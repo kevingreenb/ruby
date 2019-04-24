8ac0727d056602af2f2e5677316be762bb0cef0f
Kevin Green <kevin.e.green@hotmail.com>
2019-04-23 22:59:23 -0500

    Blog

--- Ruby/W1D1/enumerables.rb
+++ Ruby/W1D1/enumerables.rb
@@ -97,4 +97,55 @@ class Array
     end
     ret
   end
+
+  def bubble_sort!
+    sorted = false
+    while !sorted
+      sorted = true
+      i = 0 
+      while i < self.length-1
+        if self[i] > self[i+1]
+          sorted = false
+          self[i], self[i+1] = self[i+1], self[i]
+        end
+        i += 1
+      end
+    end
+    self
+  end
+
+  def bubble_sort
+    sorted = false
+    result = Array.new()
+    result = self
+    while !sorted
+      sorted = true
+      i = 0
+      while i < result.length-1
+        if result[i] > result[i+1]
+          sorted = false
+          result[i], result[i+1] = result[i+1], result[i]
+        end
+        i += 1
+      end
+    end
+    result
+  end
+
 end
+
+def factors(num)
+  ret = []
+  i = 2
+  while i < num
+    ret << i if num%i == 0
+    i+=1
+  end
+  ret
+end
+
+
+a = [2,6,5,12,-1,3,8,7,1,-4,0,23,1,-55,20,37,54,210,-23,7,483,9339,29,-3,90,-2,81,54,7372,-92,93,18,-43,21]
+p a.bubble_sort 
+p a
+
